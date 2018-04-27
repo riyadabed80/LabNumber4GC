331925bdf37f21a603238cfd08b574c16b437787
Riyad <rayabed@gmail.com>
2018-04-26 22:33:12 -0400

    First Commit

---
+++ LabNumber4GC/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace LabNumber4GC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool UserContinue = true;
+            Console.WriteLine("Learn your squares and cubes!\n");
+            while (UserContinue == true)
+
+            {
+                Console.WriteLine("Enter an integer: ");
+
+                double UserInput = (Double.Parse(Console.ReadLine()));
+
+                double Square = 0;
+                double Cube = 0;
+
+                for (double i = 1; i <= UserInput; i++)
+                {
+
+                    Square = Math.Pow(i, 2);
+                    Cube = Math.Pow(i, 3);
+
+                    Console.WriteLine("\tNumber: " + i + "\tSquared: " + Square + "\tCubed: " + Cube);
+                 
+                }
+
+                Console.WriteLine("Do you want to continue? y/n");
+                string choice = Console.ReadLine();
+                if (choice.ToLower() == "no" || choice.ToLower() == "n")
+                {
+                    UserContinue = false;
+                }
+            }
+        }
+    }
+}
